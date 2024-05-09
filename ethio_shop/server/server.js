ec2d6f434d8202b967c7668dbbaf4ee1eb94c01f
sixeyes198 <yossiguadie@icloud.com>
2024-05-09 12:11:07 +0300

    update

--- ethio_shop/server/server.js
+++ ethio_shop/server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import router from "./routes/UserRouters.js";
 const app = express();
-const path = require("path");
+import path from "path";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,13 +15,9 @@ app.listen(process.env.PORT || 3000, () => {
 app.use(router);
 
 // have Node serve the files for our built React app
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // all other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-
-// app.get("/api", (req, res) => {
-//   res.json({ Backend: "this gonna be my back-end" });
-// });

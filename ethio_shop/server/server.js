b0b40f4b65357e143cc619387756dca55f42ba0d
sixeyes198 <yossiguadie@icloud.com>
2024-05-15 15:48:12 +0300

    update

--- ethio_shop/server/server.js
+++ ethio_shop/server/server.js
@@ -13,16 +13,16 @@ app.use(cors());
 app.use(router);
 // //Resolving dir name for ES module
 // const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 // console.log(__dirname);
-// // Have Node serve the files for our built React app
-// app.use(express.static(path.join(__dirname, "/client/build")));
+// Have Node serve the files for our built React app
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-// // All other GET requests not handled before will return our React app
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
-// });
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`listening on ${process.env.PORT || 3000}`);

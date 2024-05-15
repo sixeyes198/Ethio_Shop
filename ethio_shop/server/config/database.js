86a63e90b350657986e37e1ff6a63acded9aaafd
sixeyes198 <yossiguadie@icloud.com>
2024-05-15 13:48:54 +0300

    update

--- ethio_shop/server/config/database.js
+++ ethio_shop/server/config/database.js
@@ -1,18 +1,22 @@
 import knex from "knex";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
 export const db = knex({
   client: "pg",
   connection: {
-    host: "cornelius.db.elephantsql.com",
-    port: "5432",
-    user: "vqirqwyp",
-    database: "vqirqwyp",
-    password: "rwnxctepYs7I9kxvGbLZUyVxCcNYgg3v",
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    database: DB_NAME,
+    password: DB_PASSWORD,
   },
 });
 
-// DB_HOST= cornelius.db.elephantsql.com
-// DB_PORT= 5432
-// DB_USER= vqirqwyp
-// DB_NAME= vqirqwyp
-// DB_PASSWORD= rwnxctepYs7I9kxvGbLZUyVxCcNYgg3v
+// DB_HOST = cornelius.db.elephantsql.com;
+// DB_PORT = 5432;
+// DB_USER = vqirqwyp;
+// DB_NAME = vqirqwyp;
+// DB_PASSWORD = rwnxctepYs7I9kxvGbLZUyVxCcNYgg3v;
